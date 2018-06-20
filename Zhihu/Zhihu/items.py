a6a4c8d9540c759b1b4e80d250db26817fc69823
zhuzhiming <553753991@qq.com>
2018-06-20 16:40:39 +0800

    2th

---
+++ Zhihu/Zhihu/items.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+class ZhihuItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    id = scrapy.Field() #《SO》for level 1
+    url = scrapy.Field() #《S1》评论连接 eg: https://www.zhihu.com/question/46800886/answer/102936625
+    platform = scrapy.Field() # 《S2》eg: 知乎
+    viewType = scrapy.Field() # 《S3a》eg: 问答 or 文章
+    searchWord = scrapy.Field() # 《S3b》eg:锐界
+    Title = scrapy.Field() # 《S4》eg:为什么有人41万买锐界？
+    crawlTime = scrapy.Field() # 《S5》
+    publishTime = scrapy.Field() # 《S6》
+    level = scrapy.Field() # 《S9》 1 or 2
+    commentID = scrapy.Field() # 《ID》for level 2 or "1"
+    like = scrapy.Field() # 《S13》
+    authorName = scrapy.Field() # 《G1》eg:盖子
+    authirID = scrapy.Field() # 《G0》
+    Content = scrapy.Field() # 《Q1》
+
+
---
+++ Zhihu/Zhihu/spiders/commentSP.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+from scrapy.http import Request
+from scrapy.conf import settings
+from Zhihu.items import ZhihuItem
+import scrapy
+import urllib
+import time
+import json
+import re
+
+# response.body.decode('unicode_escape')
+
+class CommentspSpider(scrapy.Spider):
+    name = 'commentSP'
+    allowed_domains = ['zhihu.com']
+    start_urls = ['http://zhihu.com/']
+    question_params = {
+        't':'general',
+        'q': None,
+        'correction': 1,
+        'offset': None,
+        'limit': 20,  #跨度
+    }
+    anserws_params = {
+        'include': 'data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,is_sticky,collapsed_by,suggest_edit,comment_count,can_comment,content,editable_content,voteup_count,reshipment_settings,comment_permission,created_time,updated_time,review_info,relevant_info,question,excerpt,relationship.is_authorized,is_author,voting,is_thanked,is_nothelp;data[*].mark_infos[*].url;data[*].author.follower_count,badge[?(type=best_answerer)].topics',
+        'offset': None,
+        'limit': 20,  #跨度
+        'sort_by': 'created'
+    }
+    comment_parmas = {
+        'include': 'data[*].author,collapsed,reply_to_author,disliked,content,voting,vote_count,is_parent_author,is_author',
+        'order': 'normal',
+        'offset': None,
+        'limit': 20,  #跨度
+        'status': 'open'
+    }
+    question_meet_end = None
+    anserws_meet_end = None
+    comment_meet_end = None
+
+    def get_localtime(self):
+        return time.strftime("%Y-%m-%d", time.localtime())
+
+    def start_requests(self):
+        for i in settings.get('SEARCHWORD'):
+            for j in range(0, settings.get('OFFSET')):
+                if not self.question_meet_end:
+                    self.question_params['q'] = i
+                    self.question_params['offset'] = j * self.question_params['limit']
+                    paramters = urllib.parse.urlencode(self.question_params)
+                    api_link = 'https://www.zhihu.com/api/v4/search_v3?{paramtes}'.format(paramtes=paramters)
+                    print('<————————————api_link————————————>\n',api_link)
+                    yield Request(url=api_link, callback=self.question_parse, meta={'kw': i})
+                else:
+                    break
+
+    def question_parse(self, response):
+        jsonBody = json.loads(response.body)
+        self.question_meet_end = jsonBody['paging']['is_end']
+        if not self.question_meet_end:
+            for item in jsonBody['data']:
+                if item['type'] == 'search_result':
+                    itemType = item['object']['type']
+                    itemCommentCount = item['object']['comment_count']
+
+                    if itemType == 'article':
+                        tmparticleTitle = item['object']['title']
+                        articleTitle = re.sub('<em>(.*)</em>',response.meta['kw'],tmparticleTitle,1)
+                        articleURL = 'https://zhuanlan.zhihu.com/p/{id}'.format(id=item['object']['id'])
+                        print(articleTitle,articleURL,itemCommentCount,'article')
+                        # yield Request(url=articleURL,callback=self.artical_page_parse,meta={'kw':response.meta['kw'],'id':item['object']['id']})
+
+                    elif itemType == 'answer' and itemCommentCount>0:
+                        tmpquestionTitle = item['object']['question']['name']
+                        questionTitle = re.sub('<em>(.*)</em>',response.meta['kw'],tmpquestionTitle,1)
+                        questionURL = 'https://www.zhihu.com/question/{id}/answers/created'.format(id=item['object']['question']['id'])
+                        # self.urlCreate_And_Send(id=item['object']['question']['id'],search_key=response.meta['kw'])
+                        print(questionTitle,questionURL,itemCommentCount,'question')
+                        self.anserws_meet_end = False
+                        self.anserws_params['offset'] = 0
+                        while not self.anserws_meet_end:
+                            paramters = urllib.parse.urlencode(self.anserws_params)
+                            questionURL = 'https://www.zhihu.com/api/v4/questions/{id}/answers?{paramtes}'.format(id=item['object']['question']['id'],paramtes=paramters)
+                            print('https://www.zhihu.com/api/v4/questions/{id}/answers  ',self.anserws_params['offset'])
+                            yield Request(url=questionURL, callback=self.answer_parse, meta={'kw': response.meta['kw']})
+                            self.anserws_params['offset'] += self.anserws_params['limit']
+                            time.sleep(3)                       #--*-- 这个数值必须大于download delay值 --*--
+                    else:
+                        print('pass a data at')
+                        continue
+        else:
+            return
+
+    def answer_parse(self, response):
+        jsonBody = json.loads(response.body)
+        self.anserws_meet_end = jsonBody['paging']['is_end']
+        if not self.anserws_meet_end:
+            for item in jsonBody['data']:
+                pipleitem = ZhihuItem()
+                pipleitem['id'] = item['id']
+                pipleitem['url'] = 'https://www.zhihu.com/question/{q_id}/answer/{a_id}'.format(q_id=item['question']['id'],a_id=item['id'])
+                pipleitem['platform'] = '知乎'
+                pipleitem['viewType'] = '问答'
+                pipleitem['searchWord'] = response.meta['kw']
+                pipleitem['Title'] = item['question']['title']
+                pipleitem['crawlTime'] = self.get_localtime()
+                pipleitem['publishTime'] = time.asctime(time.localtime(item['created_time']))
+                pipleitem['level'] = 1
+                pipleitem['commentID'] = 1
+                pipleitem['like'] = item['voteup_count']
+                pipleitem['authorName'] = item['author']['name']
+                pipleitem['authirID'] = item['author']['id']
+                pipleitem['Content'] = item['content']
+                if item['comment_count'] > 0:
+                    self.comment_meet_end = False
+                    self.comment_parmas['offset'] = 0
+                    while not self.comment_meet_end:
+                        paramas = urllib.parse.urlencode(self.comment_parmas)
+                        url = 'https://www.zhihu.com/api/v4/answers/{id}/comments?{paramters}'.format(id=item['id'],paramters=paramas)
+                        yield Request(url=url, callback=self.comment_parse, meta={'answerid':item['id'], 'kw':response.meta['kw'], 'title':item['question']['title']})
+                        self.comment_parmas['offset'] += self.comment_parmas['limit']
+                        time.sleep(3)                       #--*-- 这个数值必须大于download delay值 --*--
+                print('--------------------ANSWER--------------------\n', pipleitem,'\n--------------------ANSWER--------------------\n')
+        else:
+            return
+
+    def comment_parse(self, response):
+        jsonBody = json.loads(response.body)
+        self.comment_meet_end = jsonBody['paging']['is_end']
+        if not self.comment_meet_end:
+            for item in jsonBody['data']:
+                pipleitem = ZhihuItem()
+                pipleitem['id'] = response.meta['answerid']
+                pipleitem['commentID'] = item['id']
+                pipleitem['url'] = item['url']
+                pipleitem['platform'] = '知乎'
+                pipleitem['viewType'] = '问答'
+                pipleitem['searchWord'] = response.meta['kw']
+                pipleitem['Title'] = response.meta['title']
+                pipleitem['crawlTime'] = self.get_localtime()
+                pipleitem['publishTime'] = time.asctime(time.localtime(item['created_time']))
+                pipleitem['level'] = 2
+                pipleitem['like'] = item['vote_count']
+                pipleitem['authorName'] = item['author']['member']['name']
+                pipleitem['authirID'] = item['author']['member']['id']
+                pipleitem['Content'] = item['content']
+                print('--------------------COMMENT--------------------\n', pipleitem,'\n--------------------COMMENT--------------------\n')
+        return
+
+    def artical_parse(self, response):
+        pipleitem = ZhihuItem()
+        pipleitem['viewType'] = '文章'
+        pipleitem['id'] = response.meta['id']
+        pipleitem['url'] = response.url
+        pipleitem['platform'] = '知乎'
+        pipleitem['searchWord'] = response.meta['kw']
+        pipleitem['Title'] = response.css('.Post-Header .Post-Title').xpath('string(.)').extract_first()
+        pipleitem['crawlTime'] = self.get_localtime()
+        pipleitem['publishTime'] = response.css('.ContentItem-time span::text').extract_first()
+        pipleitem['level'] = 1
+        pipleitem['authorName'] = response.css('.AuthorInfo-name .UserLink-link').xpath('text()').extract_first()
+        pipleitem['authirID'] = response.css('.AuthorInfo-name .UserLink-link').xpath('@href').extract_first()
+        pipleitem['commentID'] = 1
+        pipleitem['Content'] = response.css('#root .Post-RichText').xpath('string(.)').extract_first()
+        # print('文章',pipleitem)
+        return
+
+
+
+
